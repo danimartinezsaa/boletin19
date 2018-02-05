b46dca09296408c9805a2ad6c05c21b7e2fca726
Daniel Martinez <dani@danielcastelao.org>
2018-02-05 09:44:49 +0100

    funcionalidades añadidas.

--- src/boletin19/Boletin19.java
+++ src/boletin19/Boletin19.java
@@ -5,6 +5,8 @@
  */
 package boletin19;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author dani
@@ -15,7 +17,42 @@ public class Boletin19{
      * @param args the command line arguments
      */
     public static void main(String[] args){
-        // TODO code application logic here
+        Libreria l=new Libreria();
+        boolean encendido=true;
+        
+        while(encendido==true){
+            int opcion=Integer.parseInt(JOptionPane.showInputDialog("MENÚ\n"+
+                    "1 - Añadir libro\n"+
+                    "2 - Vender libro\n"+
+                    "3 - Mostrar libros\n"+
+                    "4 - Eliminar libros sin unidades\n"+
+                    "5 - Consultar un libro\n"+
+                    "6 - Salir"));
+            
+            switch (opcion){
+                case 1:
+                    l.añadirLibro();
+                    break;
+                case 2:
+                    l.venderLibro();
+                    break;
+                case 3:
+                    l.mostrarLibros();
+                    break;
+                case 4:
+                    l.eliminarLibrosSinUnidades();
+                    break;
+                case 5:
+                    l.mostrarLibro();
+                    break;
+                case 6:
+                    encendido=false;
+                    System.exit(0);
+                    break;
+                default:
+                    JOptionPane.showMessageDialog(null,"Valor no válido, vuelva a introducir:");
+            }
+        }
     }
     
 }
--- src/boletin19/Libreria.java
+++ src/boletin19/Libreria.java
@@ -27,34 +27,51 @@ public class Libreria{
     
     public void venderLibro(){
         String vendido=JOptionPane.showInputDialog("Inserte nombre del libro vendido:");
-        for(Libro elemento: libros)
+        boolean bandera=true;   
+        for(Libro elemento: libros){
             if(elemento.getTitulo().equals(vendido)){
                 if(elemento.getUnidades()>=1){
                 elemento.setUnidades(elemento.getUnidades()-1);
                 JOptionPane.showMessageDialog(null,"Libro vendido.");
                 }else
-                   JOptionPane.showMessageDialog(null,"No quedan libros con ese título.");
+                   JOptionPane.showMessageDialog(null,"No quedan libros con ese título."); 
+            bandera=false;
             break;
             }
+        }
+        if(bandera==true)
+            JOptionPane.showMessageDialog(null,"No se ha encontrado ningún libro."); 
     }
     
     public void mostrarLibros(){
-        for(Libro elemento: libros)
-            System.out.println(elemento.toString());
+        if(libros.size()>=1)
+            for(Libro elemento: libros)
+                System.out.println(elemento.toString());
+        else
+            JOptionPane.showMessageDialog(null,"No hay libros para mostrar."); 
     }
     
     public void eliminarLibrosSinUnidades(){
+        boolean bandera=false;
         for(Libro elemento: libros)
             if(elemento.getUnidades()==0){
                 libros.remove(elemento);
                 JOptionPane.showMessageDialog(null,"Libros sin unidades eliminados.");
+                bandera=true;
             }
+        if(bandera==false)
+            JOptionPane.showMessageDialog(null,"No hay libros sin unidades que eliminar.");
     }
     
     public void mostrarLibro(){
+        boolean bandera=false;
         String titulo=JOptionPane.showInputDialog("Inserte título del libro: ");
         for(Libro elemento: libros)
-            if(elemento.getTitulo().equals(titulo))
+            if(elemento.getTitulo().equals(titulo)){
+                bandera=true;
                 JOptionPane.showMessageDialog(null,elemento.toString());
+            }
+        if(bandera==false)
+            JOptionPane.showMessageDialog(null,"Libro no encontrado.");
     }
 }
