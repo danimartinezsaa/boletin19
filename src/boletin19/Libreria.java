309f1e6838cd2055dc8d814222d1d583f78610ff
Daniel Martinez <dani@danielcastelao.org>
2018-02-05 09:23:14 +0100

    metodos añadidos.

--- src/boletin19/Libreria.java
+++ src/boletin19/Libreria.java
@@ -6,6 +6,7 @@
 package boletin19;
 
 import java.util.ArrayList;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -14,4 +15,46 @@ import java.util.ArrayList;
 public class Libreria{
     ArrayList<Libro> libros=new ArrayList();
     
+    public void añadirLibro(){
+        String titulo=JOptionPane.showInputDialog("Inserte el título del libro: ");
+        String autor=JOptionPane.showInputDialog("Inserte el autor:");
+        String isbn=JOptionPane.showInputDialog("Inserte ISBN:");
+        Float precio=Float.parseFloat(JOptionPane.showInputDialog("Inserte precio:"));
+        Float unidades=Float.parseFloat(JOptionPane.showInputDialog("Inserte número de unidades: "));
+        Libro libro=new Libro(titulo,autor,isbn,precio,unidades);
+        libros.add(libro);
+    }
+    
+    public void venderLibro(){
+        String vendido=JOptionPane.showInputDialog("Inserte nombre del libro vendido:");
+        for(Libro elemento: libros)
+            if(elemento.getTitulo().equals(vendido)){
+                if(elemento.getUnidades()>=1){
+                elemento.setUnidades(elemento.getUnidades()-1);
+                JOptionPane.showMessageDialog(null,"Libro vendido.");
+                }else
+                   JOptionPane.showMessageDialog(null,"No quedan libros con ese título.");
+            break;
+            }
+    }
+    
+    public void mostrarLibros(){
+        for(Libro elemento: libros)
+            System.out.println(elemento.toString());
+    }
+    
+    public void eliminarLibrosSinUnidades(){
+        for(Libro elemento: libros)
+            if(elemento.getUnidades()==0){
+                libros.remove(elemento);
+                JOptionPane.showMessageDialog(null,"Libros sin unidades eliminados.");
+            }
+    }
+    
+    public void mostrarLibro(){
+        String titulo=JOptionPane.showInputDialog("Inserte título del libro: ");
+        for(Libro elemento: libros)
+            if(elemento.getTitulo().equals(titulo))
+                JOptionPane.showMessageDialog(null,elemento.toString());
+    }
 }
