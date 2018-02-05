d4ef7238ee71e63706eef96ad6030ae294322873
Daniel Martinez <dani@danielcastelao.org>
2018-02-05 08:56:26 +0100

    Clases añadidas

---
+++ src/boletin19/Libro.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin19;
+
+/**
+ *
+ * @author dani
+ */
+public class Libro{
+    private String titulo,autor,isbn;
+    private float precio,unidades;
+
+    public Libro(){
+    }
+
+    public Libro(String titulo, String autor, String isbn, float precio, float unidades){
+        this.titulo=titulo;
+        this.autor=autor;
+        this.isbn=isbn;
+        this.precio=precio;
+        this.unidades=unidades;
+    }
+
+    public String getTitulo(){
+        return titulo;
+    }
+
+    public void setTitulo(String titulo){
+        this.titulo=titulo;
+    }
+
+    public String getAutor(){
+        return autor;
+    }
+
+    public void setAutor(String autor){
+        this.autor=autor;
+    }
+
+    public String getIsbn(){
+        return isbn;
+    }
+
+    public void setIsbn(String isbn){
+        this.isbn=isbn;
+    }
+
+    public float getPrecio(){
+        return precio;
+    }
+
+    public void setPrecio(float precio){
+        this.precio=precio;
+    }
+
+    public float getUnidades(){
+        return unidades;
+    }
+
+    public void setUnidades(float unidades){
+        this.unidades=unidades;
+    }
+
+    @Override
+    public String toString(){
+        return "titulo: "+titulo+", autor: "+autor+", isbn: "+isbn+", precio: "+precio+", unidades: "+unidades;
+    }
+    
+}
